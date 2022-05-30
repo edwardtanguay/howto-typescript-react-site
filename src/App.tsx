24f7e74d4267ef3a7416b493c21d93c21fe56745
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-30 07:29:57 +0200

    before API

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,50 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+	enum PersonStatus {
+		Customer,
+		Employee,
+		Unknown,
+	}
+
+	interface IPerson {
+		firstName: string;
+		lastName: string;
+		age: number;
+		isMember: boolean;
+		scores: number[];
+		accessGroups: string[];
+		status: PersonStatus;
+	}
+
+	const person: IPerson = {
+		firstName: 'Harald',
+		lastName: 'Deckner',
+		age: 34,
+		isMember: true,
+		scores: [95, 80, 85],
+		accessGroups: ['admins'],
+		status: PersonStatus.Employee,
+	};
+
+	return (
+		<div className="App">
+			<p>
+				The person {person.firstName} {person.lastName} has the
+				following scores {person.scores.join(', ')}.
+			</p>
+			{person.status === PersonStatus.Employee && (
+				<>
+					<p>Status is: {person.status}</p>
+					<p>Status is: {PersonStatus[person.status]}</p>
+				</>
+			)}
+		</div>
+	);
 }
 
-export default App
+export default App;
